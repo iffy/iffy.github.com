b7c08a43a8d05c4887d89e7bc64ba79f1c8e75fa
Matt Haggard <haggardii@gmail.com>
2014-10-04 22:56:56 -0600

    Fix site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://iffycan.com/iffycanblog'
+SITEURL = 'http://www.iffycan.com/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
