d050cad4e30e13e92bfa717a4fd868675e4de434
Matt Haggard <haggardii@gmail.com>
2018-05-15 17:09:22 -0600

    Thanks

---
+++ _posts/_blank.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: SOME TITLE
+# image:
+---
+
+THE BODY
+
---
+++ newpost.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME=$1
+if [ -z "$NAME" ]; then
+    echo "error: must provide a filename (just the name)"
+    exit 1
+fi
+
+FULLNAME="$(date +%Y-%m-%d)-${NAME}.md"
+
+cp _posts/_blank.md _posts/$FULLNAME
+
+subl _posts/$FULLNAME
